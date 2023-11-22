da8557b666b2cfd296c8320f243aad4249ef7903
Louie Heaton <louieheaton@icloud.com>
2023-11-22 14:44:23 +0000

    cd: add pages deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  images: {
-    unoptimized: true
-  },
-  output: 'export'
+  themeConfig: './theme.config.tsx'
 })
 
-module.exports = withNextra()
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/kapowtimeline-docs" : "";
+
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
